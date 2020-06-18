d20a7bd259a29e49b9792a969eecbbaa31404763
ruannyury <ruannyury1@outlook.com>
2020-06-18 12:31:10 -0300

    Captura de exceção

--- salario.py
+++ salario.py
@@ -1,16 +1,18 @@
 # Escrever um programa de pagamento, para pagar ao funcionário
 # 1.5 vezes o valor da taxa horária de pagamento pelo tempo
 # trabalhado acima de 40 horas.
+try: #Captura a exceção caso o usuário digite algo diferente de um numeral
+    horas = input('Digite as horas trabalhadas:\n')
+    horas = int(horas)
 
-horas = input('Digite as horas trabalhadas:\n')
-horas = int(horas)
+    taxa = input('Digite a taxa por hora:\n')
+    taxa = int(taxa)
 
-taxa = input('Digite a taxa por hora:\n')
-taxa = int(taxa)
+    if horas <= 40:
+        pagamento = horas * taxa
+    else:
+        pagamento = (taxa * 1.5) * horas
 
-if horas <= 40:
-    pagamento = horas * taxa
-else:
-    pagamento = (taxa * 1.5) * horas
-
-print('Pagamento: ', pagamento)
+    print('Pagamento: ', pagamento)
+except:
+    print('Digite apenas números!')
