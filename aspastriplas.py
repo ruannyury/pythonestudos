99b6e8ae80b13355962e8213883b3e004332cb90
ruannyury <ruannyury1@outlook.com>
2020-06-18 15:56:25 -0300

    Testing

---
+++ aspastriplas.py
@@ -0,0 +1,7 @@
+#-*- coding: utf-8 -*-
+print(''' 
+    Testando as aspas triplas para ver como fica a saída
+    do código. Este é apenas um teste.
+    Ruann Yury da Silva
+    ruannyury1@outlook.com
+''')
