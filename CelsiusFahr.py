72658061a76215148814443a3ab311a55120eb93
ruannyury <ruannyury1@outlook.com>
2020-07-07 08:33:32 -0300

    Bug corrigido

--- CelsiusFahr.py
+++ CelsiusFahr.py
@@ -1,9 +1,11 @@
 #-*- coding: utf-8 -*-
-temp = 'Digite a temperatura em Fahrenheit\n'
+def main():
+    temp = input('Digite a temperatura em Fahrenheit\n')
 #capturar uma exceção:
-try:
-    fahr = float(temp)
-    cel = (fahr - 32.0) * 5.0 / 9.0
-    print(cel)
-except:
-    print('Por favor, digite um número!')
+    try:
+        fahr = float(temp)
+        cel = (fahr - 32.0) * 5.0 / 9.0
+        print(cel)
+    except:
+        print('Por favor, digite um número!')
+main()
